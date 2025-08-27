01149ec575c63779c757a44aa8753a3a98699bba
Bilalkhan-cel <bilalahmedkhan6002@gmail.com>
2025-08-27 14:52:32 +0500

    r

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-# start
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy     
+
+app = Flask(__name__)
+
+@app.route('/')
+def home(): 
+    return "Welcome to the Passport Token System"\
+        
+        
+if __name__ == '__main__':
+    app.run(debug=True)
